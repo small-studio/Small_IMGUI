f4c36ae27fd24f52496a3c2d048901b9c91b1dcd
Mehdi-Antoine <supersmash94@tutanota.com>
2024-10-10 17:44:19 +0200

    add Small_IMGUI project : a library thats wraps imgui + low level initialization inside a simple Window class

---
+++ src/Small_IMGUI_Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Small_IMGUI_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Small_IMGUI.Window window = new Small_IMGUI.Window();
+            window.Run();
+        }
+    }
+}
